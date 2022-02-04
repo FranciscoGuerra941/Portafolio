575d01288eda29a1a88e7799cceb75f502dba532
FranciscoGuerra941 <97299394+FranciscoGuerra941@users.noreply.github.com>
2022-02-04 03:14:46 -0300

    se borra entrada de mail

--- js/form.js
+++ js/form.js
@@ -12,6 +12,6 @@ function handleSubmit(event){
     // console.log(form.get('email'));
     // console.log(form.get('textArea'));
 
-    $buttonMailto.setAttribute('href', `mailto:franciscog941@gmail.com?subject=${form.get('fname')} ${form.get('lname')} ${form.get('email')}&body=${form.get('textArea')}`);
+    $buttonMailto.setAttribute('href', `mailto:franciscog941@gmail.com?subject=${form.get('fname')} ${form.get('lname')}&body=${form.get('textArea')}`);
     $buttonMailto.click()
 }
